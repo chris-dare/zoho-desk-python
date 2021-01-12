5a353d426bbd5af2ca525d27cffebae3e905bc92
Chris Dare <engineering@chrisdare.me>
2021-01-12 22:57:33 +0200

    Implement live get tickets with ZohoDesk API

--- app/api/v1/endpoints/tickets.py
+++ app/api/v1/endpoints/tickets.py
@@ -4,10 +4,11 @@ from fastapi import APIRouter, Depends, HTTPException
 
 from app import schemas
 from app.api import deps
+from app.zoho_desk import ZohoDesk
 
 router = APIRouter()
 
 @router.get("/", response_model=List[schemas.ZohoTicket])
-def get_tickets(params: Optional[dict]= {}) -> List[schemas.ZohoTicket]:
-    return [schemas.ZohoTicket(**{"subject": "Test Ticket"})]
-
+def get_tickets(params: Optional[dict]= {}, zoho_desk: ZohoDesk = Depends(deps.get_zoho_desk_api)) -> List[schemas.ZohoTicket]:
+    tickets = zoho_desk.get_tickets()
+    return tickets
