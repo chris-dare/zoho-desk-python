8120450c7a5b952e71eca05618eb9257682f41e0
Chris Dare <engineering@chrisdare.me>
2021-01-13 11:11:57 +0200

    Implement create ticket endpoint

--- app/api/v1/endpoints/tickets.py
+++ app/api/v1/endpoints/tickets.py
@@ -12,3 +12,11 @@ router = APIRouter()
 def get_tickets(params: Optional[dict]= {}, zoho_desk: ZohoDesk = Depends(deps.get_zoho_desk_api)) -> List[schemas.ZohoTicket]:
     tickets = zoho_desk.get_tickets()
     return [schemas.ZohoTicket(**ticket) for ticket in tickets] 
+
+@router.post("/", response_model=schemas.ZohoTicket)
+def create_ticket(
+    ticket: schemas.Ticket, 
+    zoho_desk: ZohoDesk = Depends(deps.get_zoho_desk_api)
+) -> schemas.ZohoTicket:
+    zoho_ticket = zoho_desk.create_ticket(ticket)
+    return zoho_ticket
